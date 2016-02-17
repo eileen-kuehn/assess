d502cc9e57282e6d84e7f425a7aac9340455bd90
Eileen Kuehn <eileen@Eileens-MacBook-Air-3.fritz.box>
2016-02-17 17:58:12 +0100

    remove class for TreeEditDistanceAlgorithm

--- assess/algorithms/treeeditdistancealgorithm.py
+++
@@ -1,9 +0,0 @@
-from assess.algorithms.treedistancealgorithm import TreeDistanceAlgorithm
-
-
-class TreeEditDistanceAlgorithm(TreeDistanceAlgorithm):
-    def __init__(self, **kwargs):
-        TreeDistanceAlgorithm.__init__(self, **kwargs)
-
-    def _add_event(self, event, **kwargs):
-        print("adding event %s" % event)
