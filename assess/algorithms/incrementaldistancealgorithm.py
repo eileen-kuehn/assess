2dcc08536f7ac55b8981bff53e8cd7b7568ef80d
Eileen Kuehn <eileen@scc-wkitx-cl-cn-198-207.scc.kit.edu>
2016-04-13 10:22:45 +0200

    introduced signature cache

--- assess/algorithms/incrementaldistancealgorithm.py
+++ assess/algorithms/incrementaldistancealgorithm.py
@@ -2,12 +2,13 @@ from assess.algorithms.treedistancealgorithm import TreeDistanceAlgorithm
 from assess.events.events import ProcessStartEvent, ProcessExitEvent, TrafficEvent
 from assess.exceptions.exceptions import EventNotSupportedException
 from assess.algorithms.distances.simpledistance import SimpleDistance
+from assess.algorithms.signatures.signaturecache import PrototypeSignatureCache
 
 
 class IncrementalDistanceAlgorithm(TreeDistanceAlgorithm):
     def __init__(self, distance=SimpleDistance, **kwargs):
         TreeDistanceAlgorithm.__init__(self, **kwargs)
-        self._prototype_dict = {}
+        self._prototype_dict = PrototypeSignatureCache()
         self._event_counter = 0
         self._distance = distance()
         self._measured_nodes = set()
@@ -18,7 +19,11 @@ class IncrementalDistanceAlgorithm(TreeDistanceAlgorithm):
             # store links to nodes based on node_ids into dictionary
             for process in prototype.nodes():
                 signature = self._signature.get_signature(process, process.parent())
-                self._prototype_dict.setdefault(signature, set()).add(prototype)
+                self._prototype_dict.add_signature(
+                    signature=signature,
+                    prototype=prototype,
+                    value=(float(process.exit_tme)-float(process.tme))
+                )
             # initialize default distance to prototypes
             self._distance.init_distance(
                 prototypes=self.prototypes,
@@ -36,22 +41,16 @@ class IncrementalDistanceAlgorithm(TreeDistanceAlgorithm):
         if original:
             count = self._tree.node_count()
         else:
-            count = len(self._measured_nodes)
+            count = self._distance.node_count()
         return [count for _ in range(len(self._prototypes))]
 
     def node_count_for_prototype(self, prototype, original=False):
-        if original:
+        if original or self._distance.is_prototype_based_on_original():
             return prototype.node_count()
-        count = 0
-        # TODO: maybe work with a filter here
-        for values in self._prototype_dict.values():
-            for value in values:
-                if value == prototype:
-                    count += 1
-        return count
+        return self._prototype_dict.node_count(prototype=prototype)
 
     def prototypes_converted_for_algorithm(self):
-        return self._prototype_dict
+        return self._prototype_dict.internal()
 
     def _add_event(self, event, **kwargs):
         self._event_counter += 1
@@ -71,13 +70,18 @@ class IncrementalDistanceAlgorithm(TreeDistanceAlgorithm):
         # print(self._monitoring_tree.tree_repr(node_repr=lambda thenode: thenode.signature_id[self._signature]))
         return self._distance.update_distance(
             signature=signature,
-            matching_prototypes=self._prototype_dict.get(signature, set()),
+            matching_prototypes=self._prototype_dict.get(signature=signature),
             prototypes=self.prototypes
         )
 
-    def _finish_node(self, event):
-        # just ignoring the exit event of processes here
-        pass
+    def _finish_node(self, event, **kwargs):
+        signature = TreeDistanceAlgorithm._finish_node(self, event, **kwargs)
+        return self._distance.update_distance(
+            signature=signature,
+            value=float(event.tme)-float(event.start_tme),
+            matching_prototypes=self._prototype_dict.get(signature=signature),
+            prototypes=self.prototypes
+        )
 
     def finish_tree(self):
         return self._distance.finish_distance(prototypes=self._prototypes)
---
+++ assess/algorithms/signatures/signaturecache.py
@@ -0,0 +1,89 @@
+import math
+
+
+class SignatureCache(object):
+    def __init__(self):
+        self._prototype_dict = {}
+
+    def add_signature(self, signature=None):
+        if signature not in self._prototype_dict:
+            self._prototype_dict[signature] = 1
+        else:
+            self._prototype_dict[signature] += 1
+
+    def get(self, signature=None, **kwargs):
+        return self._prototype_dict.get(signature, 0)
+
+    def node_count(self, **kwargs):
+        return sum(self._prototype_dict.values())
+
+    def internal(self):
+        return self._prototype_dict
+
+
+# TODO: move it somewhere
+class MeanVariance(object):
+        def __init__(self, value=0.0):
+            self._count = 1 if value != 0 else 0
+            self._overall_count = 1
+            self._mean = value
+            self._variance = 0.0
+            # remember for faster calculation
+            self._first_part = None
+            self._second_part = None
+
+        def add(self, value=0.0):
+            self._overall_count += 1
+            if value != 0:
+                self._count += 1
+                new_mean = self._mean + (value - self._mean) / self._count
+                self._variance += (value - self._mean) * (value - new_mean)
+                self._mean = new_mean
+
+        def first_part(self):
+            """
+            curve(a*exp(-b*x*x), -3, 3)
+            :return:
+            """
+            return 1
+            # if self._first_part is None:
+            #     try:
+            #         self._first_part = 1 / (math.sqrt(2*math.pi*self._variance))
+            #     except ZeroDivisionError:
+            #         self._first_part = 0
+            # return self._first_part
+
+        def second_part(self):
+            if self._second_part is None:
+                try:
+                    self._second_part = -1 / (2*self._variance)
+                except ZeroDivisionError:
+                    self._second_part = 0
+            return self._second_part
+
+        def distance(self, value=None):
+            if value is None or value == 0:
+                return None
+            return self.first_part() * math.exp(self.second_part()*(value-self._mean)**2)
+
+        @property
+        def count(self):
+            return self._overall_count
+
+
+class PrototypeSignatureCache(SignatureCache):
+    def add_signature(self, signature=None, prototype=None, value=0.0):
+        prototype_dictionary = self._prototype_dict.setdefault(signature, dict())
+        if prototype in prototype_dictionary:
+            prototype_dictionary[prototype].add(value=value)
+        else:
+            prototype_dictionary[prototype] = MeanVariance(value=value)
+
+    def node_count(self, prototype=None):
+        # TODO: maybe work with a filter here
+        count = 0
+        for values in self._prototype_dict.values():
+            for value in values:
+                if value == prototype:
+                    count += 1
+        return count
