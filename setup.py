2fd18340ecadf057f12942252949f547ca37610f
Eileen Kuehn <eileen@scc-wkitx-cl-cn-197-26.scc.kit.edu>
2016-05-13 13:11:48 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+import setuptools
+from distutils.core import setup
+
+setup(
+    name="ASSESS",
+    version="0.1",
+    description="Comparison of different distance algorithms for dynamic trees",
+    author="Eileen Kuehn",
+    author_email="eileen.kuehn@kit.edu",
+    url="https://bitbucket.org/teamkseta/assess",
+    packages=setuptools.find_packages(),
+    dependency_links=[
+        os.path.expanduser("~/Development/libs/cachemap"),
+        os.path.expanduser("~/Development/libs/gnmutils")
+    ],
+)
