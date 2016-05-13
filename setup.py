6351255114e87c7ab0ef28c83ba343879507b942
Eileen Kuehn <eileen@scc-wkitx-cl-cn-197-26.scc.kit.edu>
2016-05-13 14:00:01 +0200

    more tests to install packages

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ setup(
     url="https://bitbucket.org/teamkseta/assess",
     packages=setuptools.find_packages(),
     dependency_links=[
-        os.path.expanduser("~/Development/python/cachemap"),
-        os.path.expanduser("~/Development/python/gnmutils"),
-        os.path.expanduser("~/Development/python/utility")
+        os.path.expanduser("git+file://~/Development/python/cachemap"),
+        os.path.expanduser("git+file://~/Development/python/gnmutils"),
+        os.path.expanduser("git+file://~/Development/python/utility")
     ],
     install_requires=["cachemap", "gnmutils", "utility"],
 )
