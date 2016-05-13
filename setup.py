c0fb2a5b0aa4bbe0a40694c328c89b3087bfbb85
Eileen Kuehn <eileen@scc-wkitx-cl-cn-197-26.scc.kit.edu>
2016-05-13 15:00:28 +0200

    added zss as requirement

--- setup.py
+++ setup.py
@@ -12,10 +12,5 @@ setup(
     author_email="eileen.kuehn@kit.edu",
     url="https://bitbucket.org/teamkseta/assess",
     packages=setuptools.find_packages(),
-    dependency_links=[
-        os.path.expanduser("git+file://~/Development/python/cachemap"),
-        os.path.expanduser("git+file://~/Development/python/gnmutils"),
-        os.path.expanduser("git+file://~/Development/python/utility")
-    ],
-    install_requires=["cachemap", "gnmutils", "utility"],
+    install_requires=["zss"],
 )
