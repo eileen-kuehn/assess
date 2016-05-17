22b43c30e6e001dfd55bde71e69f32a4ed519a93
Eileen Kuehn <eileen@Eileens-MacBook-Air-3.local>
2016-05-17 16:51:29 +0200

    added more tests for TreeDistanceAlgorithm

--- assess_tests/algorithms/test_treedistancealgorithm.py
+++ assess_tests/algorithms/test_treedistancealgorithm.py
@@ -1,6 +1,9 @@
 import unittest
 
 from assess.algorithms.treedistancealgorithm import TreeDistanceAlgorithm
+from assess.events.events import TrafficEvent
+from assess.prototypes.simpleprototypes import Prototype
+from assess.exceptions.exceptions import EventNotSupportedException
 
 
 class TestTreeDistanceAlgorithm(unittest.TestCase):
@@ -11,3 +14,18 @@ class TestTreeDistanceAlgorithm(unittest.TestCase):
     def test_update_distance(self):
         algorithm = TreeDistanceAlgorithm()
         self.assertRaises(NotImplementedError, algorithm._update_distances, None, None)
+
+    def test_traffic(self):
+        algorithm = TreeDistanceAlgorithm()
+        self.assertRaises(
+            EventNotSupportedException,
+            algorithm.add_event,
+            TrafficEvent(tme=0, pid=2, ppid=1, value=0.5)
+        )
+
+    def test_event_counts(self):
+        algorithm = TreeDistanceAlgorithm()
+        algorithm.prototypes = [Prototype()]
+        algorithm.start_tree()
+        self.assertEqual(algorithm.event_counts()[0], 0)
+        self.assertEqual(algorithm.prototype_event_counts()[0], 0)
