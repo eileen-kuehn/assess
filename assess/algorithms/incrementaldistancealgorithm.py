0b4593e9d62352c314a62d2ccda29e0f91fc313b
Eileen Kuehn <eileen@scc-wkitx-cl-cn-196-194.scc.kit.edu>
2016-05-17 10:55:03 +0200

    fixed some pylint issues for IncrementalDistanceAlgorithm

--- assess/algorithms/incrementaldistancealgorithm.py
+++ assess/algorithms/incrementaldistancealgorithm.py
@@ -1,9 +1,16 @@
+"""
+Module offers functionality to execute an incremental distance measurement for dynamic trees.
+"""
+
 from assess.algorithms.treedistancealgorithm import TreeDistanceAlgorithm
 from assess.algorithms.distances.simpledistance import SimpleDistance
 from assess.events.events import ProcessStartEvent, ProcessExitEvent
 
 
 class IncrementalDistanceAlgorithm(TreeDistanceAlgorithm):
+    """
+    The IncrementalDistanceAlgorithm takes care to adapt distance calculation for dynamic trees.
+    """
     def __init__(self, distance=SimpleDistance, **kwargs):
         TreeDistanceAlgorithm.__init__(self, **kwargs)
         self._distance = None
@@ -24,13 +31,17 @@ class IncrementalDistanceAlgorithm(TreeDistanceAlgorithm):
 
     def start_tree(self, **kwargs):
         TreeDistanceAlgorithm.start_tree(self, **kwargs)
-        self._distance.init_distance(prototypes=self.prototypes, signature_prototypes=self.signature_prototypes)
+        self._distance.init_distance(
+            prototypes=self.prototypes,
+            signature_prototypes=self.signature_prototypes
+        )
         self._measured_nodes = set()
 
     def _prototype_event_counts(self):
         if self._distance.is_prototype_based_on_original():
             return [prototype.node_count() for prototype in self._prototypes]
-        return [self._signature_prototypes.node_count(prototype=prototype) for prototype in self._prototypes]
+        return [self._signature_prototypes.node_count(prototype=prototype) for
+                prototype in self._prototypes]
 
     def _event_count(self):
         return self._distance.node_count()
