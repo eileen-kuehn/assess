2d4f023d8c790dfb3fae9418aac1d93e2d2f5ae8
Eileen Kuehn <eileen@scc-wkitx-cl-cn-196-194.scc.kit.edu>
2016-05-17 10:38:16 +0200

    fixed some pylint issues

--- assess/algorithms/distances/startexitdistance.py
+++ assess/algorithms/distances/startexitdistance.py
@@ -1,10 +1,17 @@
+"""
+The module for StartExitDistance offers classes to determine distances for dynamic trees based
+on their start and exit events.
+"""
+
 from assess.algorithms.distances.distance import Distance
 from assess.algorithms.signatures.signaturecache import SignatureCache
 
 
 class StartExitDistance(Distance):
     """
-    TODO: write description
+    The StartExitDistance offers distance measurement to a given prototype by considering start
+    and exit events. For exit events statistics like mean and variance are considered.
+    The actual distance starts at the maximum distance possible based on the prototype given.
     """
     def __init__(self, **kwargs):
         Distance.__init__(self, **kwargs)
@@ -12,7 +19,11 @@ class StartExitDistance(Distance):
         self._signature_cache = SignatureCache()
 
     def init_distance(self, prototypes=None, signature_prototypes=None):
-        Distance.init_distance(self, prototypes=prototypes, signature_prototypes=signature_prototypes)
+        Distance.init_distance(
+            self,
+            prototypes=prototypes,
+            signature_prototypes=signature_prototypes
+        )
         self._signature_cache = SignatureCache()
         for prototype in prototypes:
             self._monitoring_results_dict[prototype] = prototype.node_count()
@@ -20,11 +31,11 @@ class StartExitDistance(Distance):
     def update_distance(self, signature=None, matching_prototypes=None, prototypes=None, value=None,
                         signature_prototypes=None, **kwargs):
         self._update_distances(
-                prototype_nodes=matching_prototypes,
-                node_signature=signature,
-                prototypes=prototypes,
-                value=value,
-                signature_prototypes=signature_prototypes
+            prototype_nodes=matching_prototypes,
+            node_signature=signature,
+            prototypes=prototypes,
+            value=value,
+            signature_prototypes=signature_prototypes
         )
         self._signature_cache.add_signature(signature=signature)
         return signature
@@ -35,10 +46,12 @@ class StartExitDistance(Distance):
     def node_count(self):
         return self._signature_cache.frequency()/2.0
 
-    def _update_distances(self, prototype_nodes=None, node_signature=None, prototypes=None, value=None, signature_prototypes=None):
+    def _update_distances(self, prototype_nodes=None, node_signature=None, prototypes=None,
+                          value=None, signature_prototypes=None):
         result_dict = dict(zip(prototypes, [.5] * len(prototypes)))
         for prototype_node in prototype_nodes:
-            if self._signature_cache.get(signature=node_signature) < 2*prototype_nodes[prototype_node].count:
+            if self._signature_cache.get(signature=node_signature) < \
+                            2*prototype_nodes[prototype_node].count:
                 distance = prototype_nodes[prototype_node].distance(value=value)
                 if distance is None:
                     result_dict[prototype_node] = -.5
@@ -47,4 +60,7 @@ class StartExitDistance(Distance):
                 else:
                     result_dict[prototype_node] = distance
         # add local node distance to global tree distance
-        self._monitoring_results_dict = self._add_result_dicts(result_dict, self._monitoring_results_dict)
+        self._monitoring_results_dict = self._add_result_dicts(
+            result_dict,
+            self._monitoring_results_dict
+        )
