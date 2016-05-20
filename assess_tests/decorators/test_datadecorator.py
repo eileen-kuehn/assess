24fbe0ed3ce4de1e5c6d2444c20204d917f9ac64
Eileen Kuehn <eileen@scc-wkitx-cl-cn-192-183.scc.kit.edu>
2016-05-20 15:43:52 +0200

    added test for DataDecorator

---
+++ assess_tests/decorators/test_datadecorator.py
@@ -0,0 +1,90 @@
+import unittest
+
+from assess.decorators.datadecorator import DataDecorator
+from assess.algorithms.incrementaldistancealgorithm import IncrementalDistanceAlgorithm
+from assess.events.events import Event
+from assess_tests.basedata import simple_prototype, simple_monitoring_tree
+
+
+class TestDataDecorator(unittest.TestCase):
+    def test_creation(self):
+        decorator = DataDecorator()
+        self.assertEqual(decorator._name, "data")
+        self.assertIsNone(decorator.data())
+        self.assertEqual(
+            decorator.descriptive_data(),
+            {"data": None}
+        )
+
+    def test_decorator(self):
+        decorator = DataDecorator()
+        algorithm = IncrementalDistanceAlgorithm()
+        algorithm.prototypes = [simple_prototype()]
+        decorator.wrap_algorithm(algorithm)
+        decorator.start_tree()
+        self.assertEqual(
+            decorator.descriptive_data(),
+            {"data": {
+                "prototypes": {
+                    "original": [5],
+                    "converted": [3]
+                },
+                "monitoring": [{}]
+            }}
+        )
+        decorator.finish_tree()
+        self.assertEqual(
+            decorator.descriptive_data(),
+            {"data": {
+                "prototypes": {
+                    "original": [5],
+                    "converted": [3]
+                },
+                "monitoring": [{
+                    "original": 0,
+                    "converted": 0
+                }]
+            }}
+        )
+
+        algorithm.prototypes = [simple_prototype(), simple_prototype()]
+        decorator.wrap_algorithm(algorithm)
+
+        decorator.start_tree()
+        for event in Event.from_tree(simple_monitoring_tree()):
+            decorator.add_event(event)
+        decorator.finish_tree()
+        self.assertEqual(
+            decorator.descriptive_data(),
+            {"data": {
+                "prototypes": {
+                    "original": [5, 5],
+                    "converted": [3, 3]
+                },
+                "monitoring": [{
+                    "original": 4,
+                    "converted": 3
+                }]
+            }}
+        )
+
+        decorator.start_tree()
+        for event in Event.from_tree(simple_monitoring_tree()):
+            decorator.add_event(event)
+        decorator.finish_tree()
+        self.assertEqual(
+            decorator.descriptive_data(),
+            {"data": {
+                "prototypes": {
+                    "original": [5, 5],
+                    "converted": [3, 3]
+                },
+                "monitoring": [{
+                    "original": 4,
+                    "converted": 3
+                }, {
+                    "original": 4,
+                    "converted": 3
+                }]
+            }}
+        )
