b5669a9823b9dcd0c31cc896f26db7f867b6d287
Eileen Kuehn <eileen@scc-wkitx-cl-cn-193-3.scc.kit.edu>
2016-05-24 14:14:02 +0200

    added mode to split matrix calculations

--- assess_new.py
+++ assess_new.py
@@ -4,12 +4,14 @@ import subprocess
 import json
 import datetime
 import multiprocessing
+import time
+import shlex
+import os
 
 from utility.report import update_parser, argparse_init, LVL
 from utility.exceptions import mainExceptionFrame
 
 from assess.generators.gnm_importer import CSVEventStreamer, CSVTreeBuilder
-from assess.events.events import Event
 
 
 CLI = argparse.ArgumentParser()
@@ -74,6 +76,15 @@ CLI.add_argument(
     action="store_true",
     help="When given, the upper part (regarding a distance matrix) is not calculated"
 )
+CLI.add_argument(
+    "--output_path",
+    help="Output path to store files",
+    type=str
+)
+
+process_list = []
+max_count = 0
+current_count = 0
 
 
 def read_paths(path, maximum=None):
@@ -108,22 +119,37 @@ def main():
             prototype_paths = options.prototypes
     assert len(tree_paths) > 0
     assert len(prototype_paths) > 0
-    results = check_algorithms(
-        tree_paths=tree_paths,
-        prototype_paths=prototype_paths,
-        configurations=configdict["configurations"]
-    )
 
-    if options.json:
-        dump = {
-            "meta": {
-                "date": "%s" % datetime.datetime.now()
-            },
-            "data": results
-        }
-        print json.dumps(dump, indent=2)
+    if options.matrix:
+        for tree_index, tree_path in enumerate(tree_paths):
+            for prototype_index, prototype_path in enumerate(prototype_paths):
+                if options.no_upper and tree_index < prototype_index:
+                    break
+                if options.no_diagonal and tree_index == prototype_index:
+                    continue
+                global max_count
+                log_single_calculation(
+                    tree=tree_path,
+                    prototype=prototype_path,
+                    name="%d_%d.json" % (tree_index, prototype_index)
+                )
     else:
-        print(results)
+        results = check_algorithms(
+            tree_paths=tree_paths,
+            prototype_paths=prototype_paths,
+            configurations=configdict["configurations"]
+        )
+
+        if options.json:
+            dump = {
+                "meta": {
+                    "date": "%s" % datetime.datetime.now()
+                },
+                "data": results
+            }
+            print json.dumps(dump, indent=2)
+        else:
+            print(results)
 
 
 def do_multicore(count=1, target=None, data=None):
@@ -134,53 +160,46 @@ def do_multicore(count=1, target=None, data=None):
     return result_list
 
 
-def check_algorithm(args):
-    """
-    :param prototypes:
-    :param trees:
-    :param algorithm:
-    :param decorator:
-    :return:
-    """
-    prototypes = args.get("prototypes", None)
-    trees = args.get("trees", None)
-    algorithm = args.get("algorithm", None)
-    decorator = args.get("decorator", None)
-
-    algorithm.prototypes = prototypes
-    decorator.wrap_algorithm(algorithm=algorithm)
-    for index, path in enumerate(trees):
-        algorithm.start_tree()
-        for event in CSVEventStreamer(path):
-            algorithm.add_event(event=event)
-        algorithm.finish_tree()
-    return {
-        "algorithm": "%s" % algorithm,
-        "signature": "%s" % algorithm.signature,
-        "decorator": decorator.descriptive_data()
-    }
-
-
 def check_single_algorithm(args):
     """
-    :param prototypes:
-    :param tree:
-    :param algorithm:
-    :param decorator:
-    :return:
-    """
-    prototypes = args.get("prototypes", None)
-    tree = args.get("tree", None)
-    algorithm = args.get("algorithm", None)
-    decorator = args.get("decorator", None)
+    This method expects several parameters to be passed:
+    * signature - functor containing the signature to be created
+    * algorithm - functor containing the algorithm to be created
+    * decorator - functor containing the decorator to be created
+    * tree - path to the tree to be processed
+    * prototypes - List of prototypes
 
-    algorithm.prototypes = prototypes
+    :param args: Arguments to be passed to the method containing signature, algorithm, decorator,
+                 tree, and prototypes
+    :return: Decorator containing resulting data
+    """
+    signature = args.get("signature", None)()
+    algorithm = args.get("algorithm", None)(signature=signature)
+    algorithm.prototypes = args.get("prototypes", None)
+    decorator = args.get("decorator", None)()
     decorator.wrap_algorithm(algorithm=algorithm)
     algorithm.start_tree()
-    for event in Event.from_tree(tree):
+    for event in CSVEventStreamer(args.get("tree", None)):
         algorithm.add_event(event=event)
     algorithm.finish_tree()
     return decorator
+
+
+def log_single_calculation(tree=None, prototype=None, name=None):
+    while len(process_list) >= options.pcount:
+        for process in process_list[:]:
+            if process.poll() is not None:
+                process_list.remove(process)
+                global current_count
+                current_count += 1
+                print("finished %d / %d processes" % (current_count, max_count))
+        time.sleep(1)
+
+    command = "pypy %s --trees %s --prototypes %s --configuration %s --pcount 1 --json" % (
+        os.path.realpath(__file__), tree, prototype, options.configuration
+    )
+    filename = "%s/%s.json" % (options.output_path, name)
+    process_list.append(subprocess.Popen(shlex.split(command), stdout=open(filename, "w")))
 
 
 def check_algorithms(tree_paths=[], prototype_paths=[], configurations=[]):
@@ -194,45 +213,66 @@ def check_algorithms(tree_paths=[], prototype_paths=[], configurations=[]):
     prototypes = []
     for path in prototype_paths:
         prototypes.append(tree_builder.build(path))
-    for configuration in configurations:
-        algorithms = []
-        for algorithm in configuration["algorithms"]:
-            for signature in configuration["signatures"]:
-                alg = algorithm(signature=signature())
-                alg.prototypes = prototypes
-                algorithms.append(alg)
-        result_list = do_multicore(
-            count=options.pcount,
-            target=check_single_algorithm,
-            data=[{
-                      "prototypes": prototypes,
-                      "tree": tree_builder.build(path),
-                      "algorithm": algorithm,
-                      "decorator": configuration["decorator"]()
-                  } for algorithm in algorithms for path in tree_paths]
-        )
-        decorator = None
-        for result in result_list:
-            if decorator is not None:
-                if repr(decorator.algorithm) == repr(result.algorithm) and \
-                        repr(decorator.algorithm.signature) == repr(result.algorithm.signature):
-                    decorator.update(result)
+
+    if options.pcount > 1:
+        for configuration in configurations:
+            data = []
+            for algorithm in configuration["algorithms"]:
+                for signature in configuration["signatures"]:
+                    for path in tree_paths:
+                        data.append({
+                            "algorithm": algorithm,
+                            "signature": signature,
+                            "decorator": configuration["decorator"],
+                            "tree": path,
+                            "prototypes": prototypes
+                        })
+            result_list = do_multicore(
+                count=options.pcount,
+                target=check_single_algorithm,
+                data=data)
+            decorator = None
+            for result in result_list:
+                if decorator is not None:
+                    if repr(decorator.algorithm) == repr(result.algorithm) and \
+                            repr(decorator.algorithm.signature) == repr(result.algorithm.signature):
+                        decorator.update(result)
+                    else:
+                        # we identified a new decorator, so save the last one
+                        results["results"].append({
+                            "algorithm": "%s" % decorator.algorithm,
+                            "signature": "%s" % decorator.algorithm.signature,
+                            "decorator": decorator.descriptive_data()
+                        })
+                        decorator = result
                 else:
-                    # we identified a new decorator, so save the last one
+                    decorator = result
+            if decorator is not None:
+                results["results"].append({
+                    "algorithm": "%s" % decorator.algorithm,
+                    "signature": "%s" % decorator.algorithm.signature,
+                    "decorator": decorator.descriptive_data()
+                })
+    else:
+        for configuration in configurations:
+            for algorithm in configuration["algorithms"]:
+                for signature in configuration["signatures"]:
+                    signature_object = signature()
+                    alg = algorithm(signature=signature_object)
+                    alg.prototypes = prototypes
+                    decorator = configuration["decorator"]()
+                    decorator.wrap_algorithm(alg)
+                    for index, path in enumerate(tree_paths):
+                        alg.start_tree()
+                        for event in CSVEventStreamer(csv_path=path):
+                            alg.add_event(event=event)
+                        alg.finish_tree()
                     results["results"].append({
-                        "algorithm": "%s" % decorator.algorithm,
-                        "signature": "%s" % decorator.algorithm.signature,
+                        "algorithm": "%s" % alg,
+                        "signature": "%s" % signature_object,
                         "decorator": decorator.descriptive_data()
                     })
-                    decorator = result
-            else:
-                decorator = result
-        if decorator is not None:
-            results["results"].append({
-                "algorithm": "%s" % decorator.algorithm,
-                "signature": "%s" % decorator.algorithm.signature,
-                "decorator": decorator.descriptive_data()
-            })
+
     return results
 
 
