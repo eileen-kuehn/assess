00ec7e13b8f0192704e89901a720de8ad04babb2
Eileen Kuehn <eileen@Eileens-MacBook-Air-3.local>
2016-06-24 10:40:15 +0200

    strange uri link from gitlab

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setup(
     url="https://bitbucket.org/teamkseta/assess",
     packages=setuptools.find_packages(),
     dependency_links = ['git+ssh://git@bitbucket.org/eileenkuehn/gnmutils.git@develop#egg=gnmutils-0.1',
-                        'git+ssh://git@gitlab.scc.kit.edu:fq8360/cachemap.git@develop#egg=cachemap-0.1'],
+                        'git+ssh://git@gitlab.scc.kit.edu/fq8360/cachemap.git@develop#egg=cachemap-0.1'],
     install_requires=["zss", 'gnmutils==0.1', 'cachemap==0.1'],
 )
