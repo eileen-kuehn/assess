116befaf43c9507c5cc3fcce4ef7ad7500553125
Eileen Kuehn <eileen@Eileens-MacBook-Air-3.local>
2016-06-24 10:28:59 +0200

    adapted setup

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     author_email="eileen.kuehn@kit.edu",
     url="https://bitbucket.org/teamkseta/assess",
     packages=setuptools.find_packages(),
-    dependendy_links = ['git+ssh://git@bitbucket.org/eileenkuehn/gnmutils.git@develop#egg=gnmutils-0.1']
-    install_requires=["zss", 'gnmutils'],
+    dependendy_links = ['git+ssh://git@bitbucket.org/eileenkuehn/gnmutils.git@develop#egg=gnmutils-0.1',
+                        'git+ssh://git@gitlab.scc.kit.edu:fq8360/cachemap.git@develop#egg=cachemap-0.1'],
+    install_requires=["zss", 'gnmutils==0.1', 'cachemap==0.1'],
 )
