e7be4ab2be4a69aea69bf1a1d5700d686cc09e8d
Eileen Kuehn <eileen@scc-wkitx-cl-cn-194-178.scc.kit.edu>
2016-10-06 09:39:12 +0200

    added docstrings for pq-gram signature

--- assess/algorithms/signatures/pqgramsignature.py
+++ assess/algorithms/signatures/pqgramsignature.py
@@ -1,10 +1,13 @@
+"""
+This module provides an adapted implementation of pq-grams.
+"""
 from assess.algorithms.signatures.signatures import Signature
 
 
 class PQGramSignature(Signature):
     """
     The PQGramSignature implements the pq-gram from literature but takes some adaptations to make
-    it usable on
+    it usable on streamed tree data.
     """
     def __init__(self, height=1, width=1):
         Signature.__init__(self)
@@ -13,6 +16,13 @@ class PQGramSignature(Signature):
 
     def prepare_signature(self, node, parent):
         def parent_generator(root):
+            """
+            Generator returns names of parents in order. When no more parents are existent, an
+            empty string is returned.
+
+            :param root: The node to start at
+            :return: Parent name generator
+            """
             while True:
                 try:
                     root = root.parent()
@@ -21,6 +31,13 @@ class PQGramSignature(Signature):
                 yield root.name if root is not None else ""
 
         def sibling_generator(node):
+            """
+            Generator returns names of left siblings in order. When no more siblings to the left
+            can be found, an empty string is returned.
+
+            :param node: The node to start at
+            :return: Sibling name generator
+            """
             position = node.node_number()
             parent = node.parent()
             neighbors = parent.children_list()[0:position] if parent is not None else []
@@ -31,8 +48,7 @@ class PQGramSignature(Signature):
 
         parents = parent_generator(node)
         siblings = sibling_generator(node)
-        algorithm_id = "_".join(reversed([next(parents)
-                                for _ in range(self._height)])) + ("_%s_" % node.name) + \
-                       "_".join([next(siblings)
-                                for _ in range(self._width)])
+        algorithm_id = "_".join(reversed([next(parents) for _ in range(self._height)])) + \
+                       ("_%s_" % node.name) + \
+                       "_".join([next(siblings) for _ in range(self._width)])
         self._prepare_signature(node, algorithm_id)
