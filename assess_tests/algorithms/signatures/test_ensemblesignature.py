97c0f2105bb8f0e4b75f243142b925a1bf984f35
Eileen Kuehn <eileen.kuehn@kit.edu>
2017-01-07 19:37:26 +0100

    added test to check if ensemble signatures deliver same result as isolated calculation

--- assess_tests/algorithms/signatures/test_ensemblesignature.py
+++ assess_tests/algorithms/signatures/test_ensemblesignature.py
@@ -1,10 +1,17 @@
 import unittest
 
+from assess.algorithms.distances.startdistance import StartDistance
 from assess.algorithms.signatures.signatures import *
 from assess.algorithms.signatures.ensemblesignature import EnsembleSignature
 from assess.algorithms.incrementaldistancealgorithm import IncrementalDistanceAlgorithm
+from assess.decorators.distancematrixdecorator import DistanceMatrixDecorator
+from assess.exceptions.exceptions import EventNotSupportedException
 
-from assess_tests.basedata import simple_monitoring_tree, simple_prototype
+from assess_tests.basedata import simple_monitoring_tree, simple_prototype, real_tree
+
+from treedistancegenerator.ted_generator import TEDGenerator
+from treedistancegenerator.operations.random_operation import RandomOperation
+from treedistancegenerator.costs.costs import TreeEditDistanceCost
 
 
 class TestEnsembleSignature(unittest.TestCase):
@@ -39,3 +46,51 @@ class TestEnsembleSignature(unittest.TestCase):
         for event in simple_monitoring_tree().event_iter():
             distance = algorithm.add_event(event)
         self.assertEqual([[1], [7]], distance[-1])
+
+    def test_ensemble_result(self):
+        """
+        I recognised that apparently sometimes (or ever?) values of ensemble methods don't match
+        results from single calculations. Therefore this test should uniquely test for this.
+        """
+        tree = real_tree()
+        tree_generator = TEDGenerator(
+            operation_generator=RandomOperation(insert_probability=.5, delete_probability=.5),
+            costs=[TreeEditDistanceCost()],
+            seed=1234)
+        disturbed_tree = tree_generator.generate(tree)
+
+        signatures = [ParentChildByNameTopologySignature(), ParentCountedChildrenByNameTopologySignature(count=2)]
+        # First test results from ensemble
+        ensemble_signature = EnsembleSignature(signatures=signatures)
+        decorator = DistanceMatrixDecorator(normalized=False)
+        algorithm = IncrementalDistanceAlgorithm(
+            signature=ensemble_signature, distance=StartDistance)
+        decorator.wrap_algorithm(algorithm)
+        algorithm.prototypes = [tree]
+
+        algorithm.start_tree()
+        for event in disturbed_tree.event_iter():
+            try:
+                algorithm.add_event(event)
+            except EventNotSupportedException:
+                pass
+        algorithm.finish_tree()
+        ensemble_data = decorator.data()
+
+        # Second, validate this result with single measurements
+        single_data = {}
+        for index, signature in enumerate(signatures):
+            decorator = DistanceMatrixDecorator(normalized=False)
+            algorithm = IncrementalDistanceAlgorithm(signature=signature, distance=StartDistance)
+            decorator.wrap_algorithm(algorithm)
+            algorithm.prototypes = [real_tree()]
+            algorithm.start_tree()
+            for event in disturbed_tree.event_iter():
+                try:
+                    algorithm.add_event(event)
+                except EventNotSupportedException:
+                    pass
+            algorithm.finish_tree()
+            single_data[index] = decorator.data()
+        for index, _ in enumerate(signatures):
+            self.assertEqual(ensemble_data[0][index][0], single_data[index][0][0][0])
