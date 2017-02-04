e6bea0e5a7db9a63140a9a29da3e781a00a1ad14
Eileen Kuehn <eileen.kuehn@kit.edu>
2017-02-04 21:54:24 +0100

    enhanced versions of signature caches

--- assess/algorithms/signatures/ensemblesignaturecache.py
+++ assess/algorithms/signatures/ensemblesignaturecache.py
@@ -2,6 +2,8 @@
 This module implements ensemble version of SignatureCache as well as PrototypeSignatureCache to
 enable ensemble based algorithms.
 """
+import logging
+
 from assess.algorithms.signatures.signaturecache import SignatureCache, PrototypeSignatureCache
 from assess.events.events import ProcessStartEvent, ProcessExitEvent, TrafficEvent
 
@@ -30,24 +32,27 @@ class EnsembleSignatureCache(object):
         }
         self.statistics_cls = statistics_cls
 
-    def add_signature(self, signature=None, value=None):
-        """
-        Adding another occurence of a signature to the current cache. If signature is not in the
-        Cache so far, its count is set to 1, otherwise it is incremented.
-
-        :param signature: The signature to be added
-        """
+    def __setitem__(self, key, value):
+        signature, event_type = key
+        if not self.supported.get(event_type, False):
+            logging.getLogger(self.__class__.__name__).warning("Skipping %s (%s) for event %s" % (
+                signature, value, event_type))
+            return
         for index, token in enumerate(signature):
             try:
-                self._signature_dicts[index].add_signature(signature=token, value=value)
+                self._signature_dicts[index][token, event_type] = value
             except IndexError:
                 self._signature_dicts = [SignatureCache(self.supported, self.statistics_cls)
                                          for _ in range(len(signature))]
-                self._signature_dicts[index].add_signature(signature=token, value=value)
+                self._signature_dicts[index][token, event_type] = value
+
+    def __iter__(self):
+        for signature_dict in self._signature_dicts:
+            yield signature_dict
 
     def get(self, signature):
         """
-        Get the current counts for each signature of a given token. If token does not exist in
+        Get the current statistics for each signature of a given token. If token does not exist in
         cache, a count of 0 is returned.
 
         :param signature: Signature to get the count for
@@ -60,18 +65,26 @@ class EnsembleSignatureCache(object):
         except IndexError:
             return EnsembleSignatureCacheList()
 
-    def get_count(self, signature):
+    def multiplicity(self, signature=None, event_type=None, by_event=False):
         """
         Get the current counts for each signature of a given token. If token does not exist in
         cache, a count of 0 is returned.
+
+        If signature is None, the overall multiplicity for a given cache is given
 
         :param signature: Signature to get the count for
         :return: List of current count for tokens, otherwise []
         """
         try:
-            return EnsembleSignatureCacheList(
-                self._signature_dicts[index].get_count(signature=token) for index, token in
-                enumerate(signature))
+            if signature is None:
+                return EnsembleSignatureCacheList(cache.multiplicity(
+                    event_type=event_type, by_event=by_event) for cache in self._signature_dicts)
+            else:
+                result = EnsembleSignatureCacheList()
+                for index, token in enumerate(signature):
+                    result.append(self._signature_dicts[index].multiplicity(
+                        signature=token, event_type=event_type, by_event=by_event))
+                return result
         except IndexError:
             return EnsembleSignatureCacheList()
 
@@ -85,14 +98,14 @@ class EnsembleSignatureCache(object):
         return EnsembleSignatureCacheList(
             cache.node_count(**kwargs) for cache in self._signature_dicts)
 
-    def frequency(self):
-        """
-        Method returns the a list of the overall frequency of all tokens inside the signature
-        caches.
-
-        :return: List of accumulated count for all signatures
-        """
-        return EnsembleSignatureCacheList(cache.frequency() for cache in self._signature_dicts)
+    def get_statistics(self, signature, key, event_type):
+        try:
+            return EnsembleSignatureCacheList(
+                signature_dict.get_statistics(signature[index], key[index], event_type[index])
+                for index, signature_dict in enumerate(self._signature_dicts)
+            )
+        except IndexError:
+            return EnsembleSignatureCacheList()
 
     def internal(self):
         """
@@ -117,6 +130,24 @@ class EnsemblePrototypeSignatureCache(object):
         }
         self.statistics_cls = statistics_cls
 
+    def __iter__(self):
+        for prototype_dict in self._prototype_dict:
+            yield prototype_dict
+
+    def __setitem__(self, key, value):
+        signature, prototype, event_type = key
+        if not self.supported.get(event_type, False):
+            logging.getLogger(self.__class__.__name__).warning("Skipping %s (%s) for event %s" % (
+                signature, value, event_type))
+            return
+        for index, token in enumerate(signature):
+            try:
+                self._prototype_dict[index][token, prototype, event_type] = value
+            except IndexError:
+                self._prototype_dict = [PrototypeSignatureCache(
+                    self.supported, self.statistics_cls) for _ in range(len(signature))]
+                self._prototype_dict[index][token, prototype, event_type] = value
+
     @classmethod
     def from_prototype_signature_caches(cls, cache_list):
         if cache_list is not None and 0 < len(cache_list):
@@ -130,26 +161,6 @@ class EnsemblePrototypeSignatureCache(object):
         return EnsemblePrototypeSignatureCacheList(
             cache.signature_cache_count for cache in self._prototype_dict)
 
-    def add_signature(self, signature=None, prototype=None, value=None):
-        """
-        Add a list of tokens and its current values for a given prototype.
-
-        :param signature: List of tokens to add
-        :param prototype: Prototype the tokens belong to
-        :param value: Value for signatures
-        """
-        for index, token in enumerate(signature):
-            try:
-                self._prototype_dict[index].add_signature(signature=token,
-                                                          prototype=prototype,
-                                                          value=value)
-            except IndexError:
-                self._prototype_dict = [PrototypeSignatureCache(
-                    self.supported, self.statistics_cls) for _ in range(len(signature))]
-                self._prototype_dict[index].add_signature(signature=token,
-                                                          prototype=prototype,
-                                                          value=value)
-
     def node_count(self, prototype=None):
         """
         Returns the list of number of tokens stored for a given prototype.
@@ -162,7 +173,7 @@ class EnsemblePrototypeSignatureCache(object):
         return EnsemblePrototypeSignatureCacheList(
             cache.node_count(prototype=prototype) for cache in self._prototype_dict)
 
-    def get(self, signature=None):
+    def get(self, signature):
         """
         Returns a list of dictionaries of prototypes with their statistics for a given token.
         If the token does not exist, an empty dictionary is returned.
@@ -171,13 +182,13 @@ class EnsemblePrototypeSignatureCache(object):
         :return: List of dictionaries of prototypes with statistics as value
         """
         try:
-            return EnsembleSignatureCacheList(
+            return EnsemblePrototypeSignatureCacheList(
                 self._prototype_dict[index].get(signature=token) for index, token in
                 enumerate(signature))
         except IndexError:
             return EnsemblePrototypeSignatureCacheList()
 
-    def frequency(self, prototype=None):
+    def multiplicity(self, signature=None, prototype=None, event_type=None, by_event=False):
         """
         Returns a list of frequencies of added objects. If no prototype is given, it considers the
         frequency of all elements. Otherwise only the frequency per prototype is given.
@@ -187,5 +198,22 @@ class EnsemblePrototypeSignatureCache(object):
         :param prototype: Prototype to determine frequency from
         :return: List of frequency of tokens
         """
+        if signature is not None:
+            return EnsemblePrototypeSignatureCacheList(
+                self._prototype_dict[index].multiplicity(
+                    signature=token, prototype=prototype, event_type=event_type, by_event=by_event)
+                for index, token in enumerate(signature)
+            )
+        else:
+            return EnsemblePrototypeSignatureCacheList(
+                prototype_dict.multiplicity(prototype=prototype, event_type=event_type, by_event=by_event)
+                for prototype_dict in self._prototype_dict
+            )
+        return EnsemblePrototypeSignatureCacheList()
+
+    def get_statistics(self, signature, key, event_type, prototype):
         return EnsemblePrototypeSignatureCacheList(
-            cache.frequency(prototype=prototype) for cache in self._prototype_dict)
+            self._prototype_dict[index].get_statistics(
+                signature=token, key=key, event_type=event_type, prototype=prototype)
+            for index, token in enumerate(signature)
+        )
--- assess/algorithms/signatures/signaturecache.py
+++ assess/algorithms/signatures/signaturecache.py
@@ -2,11 +2,10 @@
 Module contains a general SignatureCache as well as a specialised for prototypes. Currently it
 also defines the statistics being available for PrototypeSignatureCache.
 """
+import logging
 
 from assess.algorithms.statistics.splittedstatistics import SplittedStatistics
-from assess.algorithms.statistics.statistics import MeanVariance
 from assess.events.events import ProcessStartEvent, ProcessExitEvent, TrafficEvent
-from cachemap.frequencycachemap import FrequencyCacheMap
 
 
 class SignatureCache(object):
@@ -15,19 +14,24 @@ class SignatureCache(object):
     statistics, e.g. MeanVariance can be supported.
     """
     def __init__(self, supported=None, statistics_cls=None):
-        self._prototype_dict = FrequencyCacheMap()
+        self._prototype_dict = {}
         self.supported = supported or {
             ProcessStartEvent: True,
             ProcessExitEvent: False,
             TrafficEvent: False
         }
+        self._keys = None
         self.statistics_cls = statistics_cls or SplittedStatistics
 
     def __setitem__(self, key, value):
-        try:
-            self._prototype_dict[key] = value
-        except KeyError:
-            self._prototype_dict[key] = value
+        signature, event_type = key
+        if not self.supported.get(event_type, False):
+            logging.getLogger(self.__class__.__name__).warning("Skipping %s (%s) for event %s" % (
+                signature, value, event_type))
+            return
+        current_value = self._prototype_dict.setdefault(signature, {}).setdefault(event_type, {})
+        for key in value:
+            current_value.setdefault(key, self.statistics_cls()).add(value[key])
 
     def __getitem__(self, item):
         return self._prototype_dict.get(item, None)
@@ -44,48 +48,25 @@ class SignatureCache(object):
     def __len__(self):
         return len(self._prototype_dict.keys())
 
-    def add_signature(self, signature=None, value=None):
-        """
-        Adding another occurence of a signature to the current cache. If signature is not in the
-        Cache so far, its count is set to 1, otherwise it is incremented.
-
-        :param signature: The signature to be added
-        :param value: The value to be added to the current signature cache
-        """
-        current_value = self._prototype_dict.get(signature, {})
-        if value:
-            for key in value:
-                current_value.setdefault(key, self.statistics_cls()).add(value[key])
-        try:
-            current_value["count"] += 1
-        except KeyError:
-            current_value["count"] = 1
-        self[signature] = current_value
+    def support_keys(self):
+        if self._keys is None:
+            self._keys = []
+            for key, value in self.supported.items():
+                if value:
+                    self._keys.append(key)
+        return self._keys
 
     def get(self, signature):
         """
-        Get the current value for given signature
+        Get the current internal statistics for given signature
 
         :param signature: Signature to get the count for
-        :return: Current count for signature, otherwise 0
+        :return: Current statistics for signature
         """
         return self[signature]
 
-    def get_count(self, signature):
-        """
-        Get the current count for a given signature. If signature does not exist in cache, a count
-        of 0 is returned.
-
-        :param signature: Signature to get the count for
-        :return: Current count for signature, otherwise 0
-        """
-        try:
-            return self._prototype_dict.get(signature, {}).get("count", 0)
-        except KeyError:
-            return 0
-
-    def get_statistics(self, signature, key):
-        pass
+    def get_statistics(self, signature, key, event_type):
+        return self._prototype_dict.get(signature, {}).get(event_type, {}).get(key, self.statistics_cls())
 
     def node_count(self, **kwargs):
         """
@@ -96,15 +77,38 @@ class SignatureCache(object):
         """
         return len(self._prototype_dict.keys())
 
-    def frequency(self):
-        """
-        Method returns the overall frequency of all signatures inside the cache.
+    def _count_for_statistics(self, statistics, event_type):
+        result = 0
+        if event_type is None:
+            # iterate over all supported keys
+            for support_key in self.support_keys():
+                result += statistics.get(support_key, {}).get("count", self.statistics_cls()).count()
+        else:
+            result += statistics.get(event_type, {}).get("count", self.statistics_cls()).count()
+        return result
 
+    def multiplicity(self, signature=None, event_type=None, by_event=False):
+        """
+        Method returns the overall multiplicity of all signatures inside the cache, either for a
+        given event_type or for all available keys.
+
+        :param event_type: The event_type to consider for determining frequency
         :return: Accumulated count for all signatures
         """
-        result = 0
-        for _, _, score in self._prototype_dict.iterscoreditems():
-            result += score
+        if by_event and event_type is None:
+            result = {}
+            for event_key in self.support_keys():
+                current_result = 0
+                for statistics in self._prototype_dict.values():
+                    current_result += self._count_for_statistics(statistics, event_key)
+                result[event_key] = current_result
+        else:
+            result = 0
+            if signature is None:
+                for statistics in self._prototype_dict.values():
+                    result += self._count_for_statistics(statistics, event_type)
+            else:
+                result += self._count_for_statistics(self._prototype_dict.get(signature, {}), event_type)
         return result
 
     def internal(self):
@@ -126,30 +130,56 @@ class PrototypeSignatureCache(SignatureCache):
         self.signature_cache_count = 1
         SignatureCache.__init__(self, supported=supported, statistics_cls=statistics_cls)
 
+    def __setitem__(self, key, value):
+        signature, prototype, event_type = key
+        if not self.supported.get(event_type, False):
+            logging.getLogger(self.__class__.__name__).warning("Skipping %s (%s) for event %s" % (
+                signature, value, event_type))
+            return
+        if signature is not None:
+            prototype_dictionary = self._prototype_dict.setdefault(signature, dict())
+            current_value = prototype_dictionary.setdefault(prototype, {}).setdefault(event_type, {})
+            for item in value:
+                current_value.setdefault(item, self.statistics_cls()).add(value=value[item])
+
     def __iadd__(self, other):
+        if type(self) != type(other):
+            return NotImplemented
         if self.supported != other.supported:
             return NotImplemented
         if self.statistics_cls != other.statistics_cls:
             return NotImplemented
-        for signature in other.internal().keys():
+        for signature in other:
             other_dict = other[signature]
-            for key in other_dict.keys():
-                try:
-                    for statistic in other_dict[key]["duration"]:
-                        for _ in range(statistic.count):
-                            self.add_signature(signature=signature,
-                                               prototype=key,
-                                               value={"duration": statistic.value})
-                    current_count = other_dict[key]["duration"].count()
-                except KeyError:
-                    current_count = 0
-                for _ in range(other_dict[key]["count"]-current_count):
-                    self.add_signature(signature=signature, prototype=key)
+            for prototype, values in other_dict.items():
+                if self.multiplicity(signature, prototype) > 0:
+                    logging.getLogger(self.__class__.__name__).warning(
+                        "skipping signature %s for prototype %s because it already exists" % (signature, prototype))
+                    continue
+                for event_type, statistics in values.items():
+                    try:
+                        for stat_key, statistic in statistics.items():
+                            current = self._prototype_dict.setdefault(signature, {}).setdefault(prototype, {}).setdefault(event_type, {}).setdefault(stat_key, self.statistics_cls())
+                            current += statistic
+                    except AttributeError:
+                        current = self._prototype_dict.setdefault(signature, {}).setdefault(prototype, {})
+                        current[event_type] = statistics
         return self
 
     @classmethod
     def from_signature_caches(cls, signature_caches, prototype=None, threshold=.1):
-        result = cls(signature_caches[0].supported)
+        """
+        Method combines several usual signature cache objects to one single prototype signature
+        cache object by taking the somehow defined average.
+
+        :param signature_caches: List of signature caches to consider
+        :param prototype: Reference of prototype to consider for resulting cache
+        :param threshold: Threshold to consider for dropping signatures
+        :return:
+        """
+        # FIXME: how can I ensure that event statistics stays stable when dropping thresholds?
+        result = cls(supported=signature_caches[0].supported, statistics_cls=signature_caches[0].statistics_cls)
+        # FIXME: I should maybe also consider that a prototype can be part of a new prototype?
         result.signature_cache_count = len(signature_caches)
         token_set = set()
         for signature_cache in signature_caches:
@@ -161,16 +191,20 @@ class PrototypeSignatureCache(SignatureCache):
             if probability > threshold:
                 # signature is getting part of prototype signature
                 # calculate average count
-                counts = sum([signature_cache.get_count(token) for
-                              signature_cache in signature_caches])
-                durations = [signature_cache[token]["duration"] for
-                             signature_cache in signature_caches if token in signature_cache]
-                prototype_dictionary = result._prototype_dict.get(token, dict())
+                collected = {}  # {supported_key: {statistics_key: [...]}}
+                for signature_cache in signature_caches:
+                    signatures = signature_cache.get(signature=token)
+                    if signatures is not None:
+                        for supported_key, statistics in signatures.items():
+                            for statistics_key, value in statistics.items():
+                                collected.setdefault(supported_key, {}).setdefault(statistics_key, []).append(value)
+                prototype_dictionary = result._prototype_dict.setdefault(token, {})
                 current_value = prototype_dictionary.setdefault(prototype, {})
-                current_value["duration"] = type(durations[0]).mean(durations)
-                current_value["count"] = int(round(counts / float(len(signature_caches))))
+                for event_type, statistics in collected.items():
+                    for statistics_key, statistic_list in statistics.items():
+                        current_value.setdefault(event_type, {})[statistics_key] = \
+                            statistic_list[0].mean(statistic_list, length=len(signature_caches))
                 current_value["probability"] = probability
-                result[token] = prototype_dictionary
         return result
 
     @classmethod
@@ -189,11 +223,10 @@ class PrototypeSignatureCache(SignatureCache):
         :param cluster_representatives: dict of cluster representatives
         :return: valid PrototypeSignatureCache object
         """
-        signature_cache = cls()
+        signature_cache = cls(supported={object: True})
         for cluster in cluster_representatives.keys():
             for element in cluster_representatives[cluster]:
-                # TODO: what about given probabilities? Can they be integrated?
-                signature_cache.add_signature(signature=element["name"], prototype=cluster)
+                signature_cache[element["name"], cluster, object] = None
         return signature_cache
 
     def add_signature(self, signature, prototype=None, value=None):
@@ -204,18 +237,7 @@ class PrototypeSignatureCache(SignatureCache):
         :param prototype: Prototype the signature belongs to
         :param value: Value for signature
         """
-        if signature is not None:
-            prototype_dictionary = self._prototype_dict.get(signature, dict())
-            current_value = prototype_dictionary.setdefault(prototype, {})
-            if value:
-                for key in value:
-                    current_value.setdefault(key, self.statistics_cls()).add(value=value[key])
-            try:
-                current_value["count"] += 1
-            except KeyError:
-                current_value["count"] = 1
-            # bumps the current count and sets current values
-            self._prototype_dict[signature] = prototype_dictionary
+        raise NotImplementedError
 
     def node_count(self, prototype=None):
         """
@@ -224,31 +246,15 @@ class PrototypeSignatureCache(SignatureCache):
         :param prototype: Prototype to get the number of signatures for
         :return: Number of signature for prototype
         """
-        # TODO: maybe work with a filter here
         if prototype is None:
             return len(self._prototype_dict)
-        count = 0
-        for values in self._prototype_dict.values():
-            for value in values:
-                if value == prototype:
-                    count += 1
-        return count
+        return sum([1 for value in self._prototype_dict.values() if prototype in value])
 
     def __getitem__(self, item):
         return self._prototype_dict.get(item, dict())
 
-    def get_count(self, signature, prototype):
-        """
-        Get the current count for a given signature. If signature does not exist in cache, a count
-        of 0 is returned.
-
-        :param signature: Signature to get the count for
-        :return: Current count for signature, otherwise 0
-        """
-        try:
-            return self._prototype_dict.get(signature, {}).get(prototype, {}).get("count", 0)
-        except KeyError:
-            return 0
+    def get_statistics(self, signature, key, event_type, prototype):
+        return self._prototype_dict.get(signature, {}).get(prototype, {}).get(event_type, {}).get(key, self.statistics_cls())
 
     def get(self, signature):
         """
@@ -260,23 +266,42 @@ class PrototypeSignatureCache(SignatureCache):
         """
         return self[signature]
 
-    def frequency(self, prototype=None):
+    def multiplicity(self, signature=None, prototype=None, event_type=None, by_event=False):
         """
         Returns the frequency of added objects. If no prototype is given, it considers the frequency
         of all elements. Otherwise only the frequency per prototype is given.
+        This can further be detailed by specifying signature or event_type.
 
+        :param signature: Signature to determine frequency from
         :param prototype: Prototype to determine frequency from
+        :param event_type: Event_type to determine frequency from
         :return: Frequency of signatures
         """
         result = 0
         if prototype is not None:
-            for value in self._prototype_dict.values():
-                try:
-                    result += value.get(prototype, {})["count"]
-                except KeyError:
-                    pass
+            if by_event and event_type is None:
+                result = {}
+                for event_key in self.support_keys():
+                    current_result = 0
+                    for signatures in self._prototype_dict.values():
+                        current_result += self._count_for_statistics(signatures.get(prototype, {}), event_key)
+                    result[event_key] = current_result
+            else:
+                if signature is None:
+                    for signatures in self._prototype_dict.values():
+                        result += self._count_for_statistics(signatures.get(prototype, {}), event_type)
+                else:
+                    current_dict = self._prototype_dict.get(signature, {})
+                    result += self._count_for_statistics(current_dict.get(prototype, {}), event_type)
         else:
-            for prototype_dict in self._prototype_dict.values():
-                for value_dict in prototype_dict.values():
-                    result += value_dict.get("count", 0)
+            if by_event and event_type is None:
+                raise NotImplementedError
+            if signature is None:
+                for prototype_dict in self._prototype_dict.values():
+                    for statistics in prototype_dict.values():
+                        result += self._count_for_statistics(statistics, event_type)
+            else:
+                current_dict = self._prototype_dict.get(signature, {})
+                for statistics in current_dict.values():
+                    result += self._count_for_statistics(statistics, event_type)
         return result
