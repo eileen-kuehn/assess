31d734f4ed3631ed47224f89bb06d42b3838fafa
Eileen Kuehn <eileen.kuehn@kit.edu>
2017-04-14 22:04:49 +0200

    adapted ensemble results to matrix result

--- assess/decorators/ensemblematrixdecorator.py
+++ assess/decorators/ensemblematrixdecorator.py
@@ -2,8 +2,8 @@
 This module offers an implementation to output the different distances for dynamic trees based
 on the single events that are processed. The actual output is in form of a vector.
 """
-
 from assess.decorators.decorator import Decorator
+from assess.exceptions.exceptions import MatrixDoesNotMatchBounds
 
 
 class EnsembleMatrixDecorator(Decorator):
@@ -15,9 +15,13 @@ class EnsembleMatrixDecorator(Decorator):
 
     The results are given in the following format:
     [                               <- start of matrix
-        [p1t1, ..., pnt1]           <- list of distance events per prototype considering all ensembles
+        [                           <- start of a tree
+            [p1t1, ..., pnt1]       <- only one ensemble is relevant here
+        ],
         ...
-        [p1tn, ..., pntn]
+        [
+            [p1tn, ..., pntn]
+        ]
     ]
     """
     def __init__(self, normalized=False):
@@ -25,7 +29,7 @@ class EnsembleMatrixDecorator(Decorator):
             Decorator.__init__(self, name="normalized_ensembles")
         else:
             Decorator.__init__(self, name="ensembles")
-        self._data = []
+        self._data = None
         self._normalized = normalized
         # format: [[e1p1, ..., e1pn], ..., [enp1, ..., enpn]]
         self._tmp_prototype_counts = None
@@ -39,18 +43,25 @@ class EnsembleMatrixDecorator(Decorator):
         self._tmp_prototype_counts = None
 
     def _tree_started(self):
+        size = self._matrix_size()
+        if self._data and size < len(self._data) + 1:
+            raise MatrixDoesNotMatchBounds(size, size, len(self._data) + 1)
         if self._tmp_prototype_counts is None:
             self._tmp_prototype_counts = self._algorithm.prototype_event_counts()
+        # add a new row for a new algorithm for only _one_ ensemble
+        self._data.append([[prototype for prototype in self._tmp_prototype_counts[0]]])
         self._last_distance_event = self._tmp_prototype_counts
-        self._data.append([prototype for prototype in self._tmp_prototype_counts[0]])
+        # FIXME: self._data.append([prototype for prototype in self._tmp_prototype_counts[0]])
 
     def _tree_finished(self, result):
         event_counts = self._algorithm.event_counts()  # [[e1p1, ..., e1pn], ..., [enp1, ..., enpn]]
+
+        i=0
         for j, prototype_result in enumerate(event_counts[0]):
             if self._normalized:
-                distance = self._data[-1][j]
-                self._data[-1][j] = 2 * distance / float(
-                    event_counts[0][j] + self._tmp_prototype_counts[0][j] + distance)
+                distance = self._data[-1][i][j]
+                self._data[-1][i][j] = 2 * distance / float(
+                    event_counts[i][j] + self._tmp_prototype_counts[i][j] + distance)
             else:
                 pass
 
@@ -68,14 +79,56 @@ class EnsembleMatrixDecorator(Decorator):
                 tmp_differences[j] += result[i][j] - self._last_distance_event[i][j]
         # then divide by number of ensembles to get mean
         tmp_differences = [value / float(len(self._tmp_prototype_counts)) for value in tmp_differences]
+        ensemble_idx = 0
         for index, difference in enumerate(tmp_differences):
-            self._data[-1][index] += difference
+            self._data[-1][ensemble_idx][index] += difference
         self._last_distance_event = result
+
+    def _matrix_size(self):
+        if self._data is None:
+            self._data = [[]]
+        if len(self._data) > 0:
+            return len(self._algorithm.prototypes)
+        return 0
 
     def _update(self, decorator):
         self._data.extend(decorator.data())
 
     def __iadd__(self, other):
+        """
+        [                           [
+            [                           [                               tree_idx
+                [p1t1e1, p2t1e1],           [p3t1e1, ..., pnt1e1],      ensemble_idx
+                ...,                        ...,
+                [p1t1en, p2t1en]            [p3t1en, ..., pnt1en]
+            ],                          ],
+            ...                         ...
+            [                           [
+                [p1tne1, p2tne1],           [p3tne1, ..., pntne1],
+                ...                         ...,
+                [p1tnen, p2tnen]            [p3tnen, ..., pntnen]
+            ]                           ]
+        ]                           ]
+        :param other:
+        :return:
+        """
+        try:
+            if other.row_idx[0] > self.row_idx[0]:
+                # append new trees
+                self._data.extend(other._data)
+                self.row_idx[0] = other.row_idx[0]
+                # as we are starting a new row, we can also take col index
+                self.col_idx[0] = other.col_idx[0]
+            elif other.row_idx[0] == self.row_idx[0] and other.col_idx[0] > self.col_idx[0]:
+                # append new prototypes
+                for tree_idx, tree_values in enumerate(other._data):
+                    for ensemble_idx, ensemble_values in enumerate(tree_values):
+                        self._data[-(len(other._data) - tree_idx)][ensemble_idx].extend(ensemble_values)
+                self.col_idx[0] = other.col_idx[0]
+            return self
+        except AttributeError:
+            pass
         for tree_idx, tree_values in enumerate(other._data):
-            self._data[tree_idx].extend(tree_values)
+            for ensemble_idx, ensemble_values in enumerate(tree_values):
+                self._data[tree_idx][ensemble_idx].extend(ensemble_values)
         return self
