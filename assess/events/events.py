405f96a821dfaa55fee8c11ea693795fba25a20b
Eileen Kuehn <eileen.kuehn@kit.edu>
2019-07-25 00:20:20 +0200

    Update assess/events/events.py
    Co-Authored-By: Max Fischer <maxfischer2781@gmail.com>

--- assess/events/events.py
+++ assess/events/events.py
@@ -66,7 +66,7 @@ class Event(object):
             process_dict["pid"] = process_dict.get("node_id", None)
             process_exit_dict["pid"] = process_dict.get("node_id", None)
             if process_dict.get("_parent", None) is not None:
-                ppid = process_dict.get("_parent", None).node_id
+                ppid = process_dict["_parent"].node_id
                 process_dict["ppid"] = ppid
                 process_exit_dict["ppid"] = ppid
             else:
